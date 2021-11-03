d10a928fa62a6ba3c9ede936227f386eb51bd1d4
Yuliya Kazakova <julia.kazakova23@gmail.com>
2021-11-03 03:56:18 +0300

    Fix modal on mobile

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,4 +1,4 @@
-import React, { useContext, useMemo } from 'react';
+import React, { useContext } from 'react';
 import Pagination from './Pagination';
 import MovieModal from './MovieModal';
 import { getImageSrc } from '../utils';
